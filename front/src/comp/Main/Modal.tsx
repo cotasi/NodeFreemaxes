bd9ae3b32d6f573ad0445985bce8eef1586dcf08
cotasi135 <cotasi135@gmail.com>
2024-03-03 18:46:21 +0900

    최종

--- front/src/comp/Main/Modal.tsx
+++ front/src/comp/Main/Modal.tsx
@@ -9,11 +9,11 @@ const Modal = () => {
         })
     })
     return (
-        <div className="modal" style={{width: "60%", height: "80%",backgroundColor: "white", position: "fixed", left: "50%", transform: 'translateX(-50%)', top: '3%', zIndex: '80'}}>
-            <div className="img" style={{height: '90%'}}><img src="https://i.imgur.com/UW4I4yc.png" alt="modal" style={{width: '100%', height: '100%', objectFit: 'cover'}}/></div>
+        <div className="modal" style={{width: "300px", height: "500px",backgroundColor: "white", position: "fixed", left: "50%", transform: 'translate(-50%,-50%)', top: '50%', zIndex: '80'}}>
+            <div className="img" style={{height: '90%'}}><img src="/images/광고배너.jpg" alt="modal" style={{width: '100%', height: '100%', objectFit: 'cover'}}/></div>
             <div className="btns" style={{position: 'relative', height: '10%', display: 'flex'}}>
-                <button>오늘 더 이상 띄우지 않기</button>
-                <button style={{position: 'absolute', top: '50%', right: '0', transform: 'translateY(-50%)', paddingRight: '2rem'}}>닫기</button>
+                <button style={{width: '50%', fontSize:'.8rem', borderRight: '1px solid rgba(0,0,0,.2)'}}>오늘 더 이상 띄우지 않기</button>
+                <button style={{width: '50%', fontSize:'.8rem'}}>닫기</button>
             </div>
         </div>
     );
